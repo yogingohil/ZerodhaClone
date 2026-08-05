a234fcd23bb1a98e08b90169730fb17ac7653348
yogingohil <gohilyogin123@gmail.com>
2026-08-05 17:39:40 +0530

    Add CI=false and DISABLE_ESLINT_PLUGIN=true for Vercel builds

---
+++ frontend/src/test/Hero.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Hero from '../landing_page/home/Hero';
+
+describe("Hero component", () =>{
+    test("Render hero image",() =>{
+        render(<Hero />);
+        const heroImage = screen.getByAltText("Hero Image");
+        expect(heroImage).toBeInTheDocument();
+        expect(heroImage).toHaveAttribute("src", "media/images/homeHero.png");
+    })
+})
+
+
